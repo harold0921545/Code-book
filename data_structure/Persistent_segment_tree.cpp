ac12bb715e3187a41342d526dd300fd0851621b5
harold0921545 <haroldlin.0921@gmail.com>
2022-09-06 23:07:48 +0800

    add Persistent_segment_tree.cpp

---
+++ data_structure/Persistent_segment_tree.cpp
@@ -0,0 +1,38 @@
+struct Persistent_ST{
+    int rt[N * 20], lc[N * 20], rc[N * 20], seg[N * 20], idx;
+
+    void build(int l,int r,int &p, int *data){
+        p = ++idx;
+        if(l == r){
+            seg[p] = data[l];
+            return;
+        }
+        int mid = l + r >> 1;
+        build(l, mid, lc[p]);
+        build(mid + 1, r, rc[p]);
+    }
+
+    void upd(int l,int r,int &p,int pre,int q,int k){
+        p = ++idx;
+        lc[p] = lc[pre], rc[p] = rc[pre], seg[p] = seg[pre];
+        if(l == r){
+            seg[p] = k;
+            return;
+        }
+        int mid = l + r >> 1;
+        if (q <= mid)
+            upd(l, mid, lc[p], lc[pre], q, k);
+        else
+            upd(mid + 1, r, rc[p], rc[pre], q, k);
+    }
+
+    int qy(int l,int r,int p,int q){
+        if (l == r)
+            return seg[p];
+        int mid = l + r >> 1;
+        if (q <= mid)
+            return qy(l, mid, lc[p], q);
+        else
+            return qy(mid + 1, r, rc[p], q);
+    }
+};
