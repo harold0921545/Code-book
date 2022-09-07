2918aef905652d463dc952160e38fe271b955059
harold0921545 <haroldlin.0921@gmail.com>
2022-09-07 18:51:59 +0800

    add Z_algorithm.cpp

---
+++ string/Z_algorithm.cpp
@@ -0,0 +1,13 @@
+int z[N];
+vector<int> Z_val(string s){
+    int l = 0, r = 0;
+    vector<int> ans;
+    for (int i = 0; i < s.size(); i++){
+        z[i] = max(0, min(z[i - l], r - i));
+        while (i + z[i] < s.size() && s[z[i]] == s[i + z[i]])
+            l = i, z[i]++, r = i + z[i];
+        if (i + z[i] == s.size())
+            ans.pb(s.size() - i);
+    }
+    return ans;
+}
