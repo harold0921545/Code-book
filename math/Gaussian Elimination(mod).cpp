d7b9c382a65afc60fd303bbd0d602ce0348454ff
harold0921545 <haroldlin.0921@gmail.com>
2022-09-24 17:04:25 +0800

    add Gaussian Elimination(mod).cpp

---
+++ math/Gaussian Elimination(mod).cpp
@@ -0,0 +1,35 @@
+int a[1005][1005];
+int n, MOD;
+int fpow(int x, int p){
+    int r = 1;
+    for (; p; p >>= 1){
+        if (p & 1)
+            (r *= x) %= MOD;
+        (x *= x) %= MOD;
+    }
+    return r;
+}
+void gaussian_elimination_mod(){
+    for (int i = 1; i <= n; i++){
+        if (a[i][i] == 0){
+            int tmp = i;
+            for (int j = i + 1; j <= n; j++)
+                if (a[j][i]){
+                    tmp = j;
+                    break;
+                }
+            for (int j = 1; j <= n + 1; j++)
+                swap(a[i][j], a[tmp][j]);
+        }
+        int tmp = a[i][i];
+        for (int j = i; j <= n + 1; j++)
+            (a[i][j] *= fpow(tmp, MOD - 2)) %= MOD;
+        for (int j = 1; j <= n; j++){
+            if (i == j) continue;
+            tmp = a[j][i];
+            for (int k = 1; k <= n + 1; k++)
+                a[j][k] = ((a[j][k] - a[i][k] * tmp % MOD) + MOD) % MOD;
+        }
+    }
+    // x_i = a[i][n + 1]
+}
