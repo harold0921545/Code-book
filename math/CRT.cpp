dc4a36b161594427db42f2babd52d7748bd6b852
harold0921545 <haroldlin.0921@gmail.com>
2023-01-24 19:42:11 +0800

    fix CRT.cpp

--- math/CRT.cpp
+++ math/CRT.cpp
@@ -1,14 +1,12 @@
-// ans % m[i] == a[i]
-int m[N],a[N],mi[N], n;
-int CRT(){
-    int M = 1, ans =0;
-    for(int i = 1; i <= n; i++)
+// res % m[i] == r[i]
+int CRT(int n, int *m, int *r){
+    int M = 1, res = 0;
+    for (int i = 1; i <= n; i++)
         M *= m[i];
-    for(int i = 1; i <= n; i++){
-        mi[i] = M / m[i];
-        pii tmp = exgcd(mi[i],m[i]);
-        int x = tmp.F, y = tmp.S;
-        ans += a[i] * mi[i] * (x < 0? x + m[i]: x);
+    for (int i = 1; i <= n; i++){
+        int tm = M / m[i], x, y;
+        exgcd(tm, m[i], x, y);
+        res = ((res + r[i] * tm * x % M)) % M;
     }
-    return ans % M;
+    return (res + M) % M;
 }
