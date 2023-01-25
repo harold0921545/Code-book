7cc72ef6f730db4dab342561bcbca8fed5ad59b8
harold0921545 <haroldlin.0921@gmail.com>
2023-01-25 18:56:32 +0800

    fix Trie.cpp

--- data_structure/Trie.cpp
+++ data_structure/Trie.cpp
@@ -1,36 +1,36 @@
 int idx, cnt[N];
 struct Trie{
     int ch[26];
-    void add(string s){
-        int u = 0;
-        for (int i = 0; i < s.size(); i++){
-            int w = s[i] - 'a';
-            if (tr[u].ch[w] == 0)
-                tr[u].ch[w] = ++idx;
-            u = tr[u].ch[w];
-            cnt[u]++;
-        }
-    }
-    void del(string s){
-        int u = 0;
-        for (int i = 0; i < s.size(); i++){
-            int w = s[i] - 'a';
-            int nxt = tr[u].ch[w];
-            cnt[nxt]--;
-            if (cnt[nxt] == 0)
-                tr[u].ch[w] = 0;
-            u = nxt;
-        }
-    }
-    bool match(string s){
-        int u = 0;
-        for (int i = 0; i < s.size(); i++){
-            int w = s[i] - 'a';
-            if (cnt[tr[u].ch[w]] > 0)
-                u = tr[u].ch[w];
-            else 
-                return false;
-        }
-        return true;
-    }
 } tr[N];
+void add(string s){
+    int u = 0;
+    for (int i = 0; i < s.size(); i++){
+        int w = s[i] - 'a';
+        if (tr[u].ch[w] == 0)
+            tr[u].ch[w] = ++idx;
+        u = tr[u].ch[w];
+        cnt[u]++;
+    }
+}
+void del(string s){
+    int u = 0;
+    for (int i = 0; i < s.size(); i++){
+        int w = s[i] - 'a';
+        int nxt = tr[u].ch[w];
+        cnt[nxt]--;
+        if (cnt[nxt] == 0)
+            tr[u].ch[w] = 0;
+        u = nxt;
+    }
+}
+bool match(string s){
+    int u = 0;
+    for (int i = 0; i < s.size(); i++){
+        int w = s[i] - 'a';
+        if (cnt[tr[u].ch[w]] > 0)
+            u = tr[u].ch[w];
+        else 
+            return false;
+    }
+    return true;
+}
