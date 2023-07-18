a9b072bb7d523faec610ec8d8b4a6efc6fae470e
sha_man <haroldlin.0921@gmail.com>
2023-07-18 23:06:30 +0800

    fix tab

--- data_structure/link_cut_tree.cpp
+++ data_structure/link_cut_tree.cpp
@@ -1,4 +1,4 @@
-struct Node {
+struct Node{
 	int x;
 	Node *l = 0;
 	Node *r = 0;
@@ -9,63 +9,71 @@ struct Node {
 
 	Node(int v) { x = v; }
 
-	void push() {
-		if (rev) {
+	void push(){
+		if (rev){
 			rev = false;
 			swap(l, r);
-			if (l) l->rev ^= true;
-			if (r) r->rev ^= true;
+			if (l)
+				l->rev ^= true;
+			if (r)
+				r->rev ^= true;
 		}
 	}
 
 	bool is_root() { return p == 0 || (p->l != this && this != p->r); }
 };
 
-struct LCT {
+struct LCT{
 	vector<Node> a;
 
-	LCT(int n) {
+	LCT(int n){
 		a.resize(n + 1);
-		for (int i = 1; i <= n; ++i) a[i].x = i;
+		for (int i = 1; i <= n; ++i)
+			a[i].x = i;
 	}
 
-	void rot(Node *c) {
+	void rot(Node *c){
 		auto p = c->p;
 		auto g = p->p;
 
-		if (!p->is_root()) (g->r == p ? g->r : g->l) = c;
+		if (!p->is_root())
+			(g->r == p ? g->r : g->l) = c;
 
 		p->push();
 		c->push();
 
-		if (p->l == c) {  // rtr
+		if (p->l == c){ // rtr
 			p->l = c->r;
 			c->r = p;
-			if (p->l) p->l->p = p;
-		} else {  // rtl
+			if (p->l)
+				p->l->p = p;
+		}
+		else{ // rtl
 			p->r = c->l;
 			c->l = p;
-			if (p->r) p->r->p = p;
+			if (p->r)
+				p->r->p = p;
 		}
 
 		p->p = c;
 		c->p = g;
 	}
 
-	void splay(Node *c) {
-		while (!c->is_root()) {
+	void splay(Node *c){
+		while (!c->is_root()){
 			auto p = c->p;
 			auto g = p->p;
-			if (!p->is_root()) rot((g->r == p) == (p->r == c) ? p : c);
+			if (!p->is_root())
+				rot((g->r == p) == (p->r == c) ? p : c);
 			rot(c);
 		}
 		c->push();
 	}
 
-	Node *access(int v) {
+	Node *access(int v){
 		Node *last = 0;
 		Node *c = &a[v];
-		for (Node *p = c; p; p = p->p) {
+		for (Node *p = c; p; p = p->p){
 			splay(p);
 			p->r = last;
 			last = p;
@@ -74,22 +82,23 @@ struct LCT {
 		return last;
 	}
 
-	void make_root(int v) {
+	void make_root(int v){
 		access(v);
 		auto *c = &a[v];
-		if (c->l) c->l->rev ^= true, c->l = 0;
+		if (c->l)
+			c->l->rev ^= true, c->l = 0;
 	}
 
-	void link(int u, int v) {
+	void link(int u, int v){
 		make_root(v);
 		Node *c = &a[v];
 		c->p = &a[u];
 	}
 
-	void cut(int u, int v) {
+	void cut(int u, int v){
 		make_root(u);
 		access(v);
-		if (a[v].l) {
+		if (a[v].l){
 			a[v].l->p = 0;
 			a[v].l = 0;
 		}
@@ -106,7 +115,7 @@ struct LCT {
 		return p->x;
 	}
 
-	bool connected(int u, int v) {
+	bool connected(int u, int v){
 		return Find(u) == Find(v);
 	}
 };
