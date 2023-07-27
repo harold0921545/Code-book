ae09b05d214379007c0d6d71350e1093b0eb8ad8
sha_man <haroldlin.0921@gmail.com>
2023-07-27 18:46:08 +0800

    fix link_cut_tree.cpp

--- data_structure/link_cut_tree.cpp
+++ data_structure/link_cut_tree.cpp
@@ -1,121 +1,118 @@
-struct Node{
-	int x;
-	Node *l = 0;
-	Node *r = 0;
-	Node *p = 0;
-	bool rev = false;
+#include <bits/stdc++.h>
+using namespace std;
+#define MP make_pair
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
 
-	Node() = default;
+#define F first
+#define S second
 
-	Node(int v) { x = v; }
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+#define noTLE ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define debug(x) cerr << #x << " = " << x << '\n'
 
-	void push(){
-		if (rev){
-			rev = false;
-			swap(l, r);
-			if (l)
-				l->rev ^= true;
-			if (r)
-				r->rev ^= true;
-		}
-	}
-
-	bool is_root() { return p == 0 || (p->l != this && this != p->r); }
-};
+const int N = 1e5 + 25;
 
 struct LCT{
-	vector<Node> a;
-
-	LCT(int n){
-		a.resize(n + 1);
-		for (int i = 1; i <= n; ++i)
-			a[i].x = i;
-	}
-
-	void rot(Node *c){
-		auto p = c->p;
-		auto g = p->p;
-
-		if (!p->is_root())
-			(g->r == p ? g->r : g->l) = c;
-
-		p->push();
-		c->push();
-
-		if (p->l == c){ // rtr
-			p->l = c->r;
-			c->r = p;
-			if (p->l)
-				p->l->p = p;
+	int top, c[N][2], fa[N], xr[N], q[N], rev[N], val[N];
+	void pull(int x) { xr[x] = xr[c[x][0]] ^ xr[c[x][1]] ^ val[x]; }
+	void push(int x){
+		if(rev[x]){
+			rev[c[x][0]] ^= 1;
+			rev[c[x][1]] ^= 1;
+			rev[x] = 0;
+			swap(c[x][0], c[x][1]);
 		}
-		else{ // rtl
-			p->r = c->l;
-			c->l = p;
-			if (p->r)
-				p->r->p = p;
+    }
+	bool isroot(int x) { return c[fa[x]][0] != x && c[fa[x]][1] != x; }
+	void rotate(int x){
+		int y = fa[x], z = fa[y], l, r;
+		if (c[y][0] == x)
+			l = 0;
+		else
+			l = 1;
+		r = l ^ 1;
+		if (!isroot(y)){
+			if (c[z][0] == y)
+				c[z][0] = x;
+			else
+				c[z][1] = x;
 		}
-
-		p->p = c;
-		c->p = g;
+		fa[x] = z, fa[y] = x, fa[c[x][r]] = y;
+		c[y][l] = c[x][r];
+		c[x][r] = y;
+		pull(y), pull(x);
 	}
-
-	void splay(Node *c){
-		while (!c->is_root()){
-			auto p = c->p;
-			auto g = p->p;
-			if (!p->is_root())
-				rot((g->r == p) == (p->r == c) ? p : c);
-			rot(c);
-		}
-		c->push();
-	}
-
-	Node *access(int v){
-		Node *last = 0;
-		Node *c = &a[v];
-		for (Node *p = c; p; p = p->p){
-			splay(p);
-			p->r = last;
-			last = p;
-		}
-		splay(c);
-		return last;
-	}
-
-	void make_root(int v){
-		access(v);
-		auto *c = &a[v];
-		if (c->l)
-			c->l->rev ^= true, c->l = 0;
-	}
-
-	void link(int u, int v){
-		make_root(v);
-		Node *c = &a[v];
-		c->p = &a[u];
-	}
-
-	void cut(int u, int v){
-		make_root(u);
-		access(v);
-		if (a[v].l){
-			a[v].l->p = 0;
-			a[v].l = 0;
+    void splay(int x){
+		top = 1;
+		q[top] = x;
+		for (int i = x; !isroot(i); i = fa[i])
+			q[++top] = fa[i];
+		for (int i = top; i; i--)
+			push(q[i]);
+		while (!isroot(x)){
+			int y = fa[x], z = fa[y];
+			if (!isroot(y)){
+				if ((c[y][0] == x) ^ (c[z][0] == y))
+					rotate(x);
+				else
+					rotate(y);
+			}
+			rotate(x);
 		}
 	}
-
-	int Find(int u){
-		access(u);
-		Node *p = &a[u];
-		splay(p);
-		p->push();
-		while (p->l)
-			p = p->l, p->push();
-		splay(p);
-		return p->x;
+    void access(int x){
+		for (int t = 0; x; t = x, x = fa[x])
+			splay(x), c[x][1] = t, pull(x);
 	}
-
-	bool connected(int u, int v){
-		return Find(u) == Find(v);
+	void makeroot(int x) { access(x), splay(x), rev[x] ^= 1; }
+	int find(int x){
+		access(x), splay(x);
+		while (c[x][0])
+			x = c[x][0];
+		return x;
 	}
-};
+	void split(int x, int y) { makeroot(x), access(y), splay(y); }
+	void cut(int x, int y){
+		split(x, y);
+		if (c[y][0] == x && c[x][1] == 0)
+			c[y][0] = 0, fa[x] = 0;
+	}
+	void link(int x, int y){
+		makeroot(x);
+		fa[x] = y;
+	}
+	bool connected(int x, int y) { return find(x) == find(y); }
+} lct;
+
+
+signed main(){
+	noTLE;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i)
+		cin >> lct.val[i];
+	while (m--){
+		int opt, x, y;
+		cin >> opt >> x >> y;
+		if (opt == 0){ // xor between x and y
+			lct.split(x, y);
+			cout << lct.xr[y] << '\n';
+		}
+		else if (opt == 1){
+			if (!lct.connected(x, y))
+				lct.link(x, y);
+		}
+		else if (opt == 2){
+			if (lct.connected(x, y))
+				lct.cut(x, y);
+		}
+		else // change value
+			lct.access(x), lct.splay(x), lct.val[x] = y, lct.pull(x);
+	}
+}
